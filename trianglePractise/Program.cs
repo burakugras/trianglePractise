072a18f871107c433d31a1362a044c234e2c98ae
burakugras <99265170+burakugras@users.noreply.github.com>
2022-03-07 16:36:14 +0300

    Add files via upload

---
+++ trianglePractise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trianglePractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the base length of triangle:");
+            int baseSide = int.Parse(Console.ReadLine());
+            triangle(baseSide);
+
+            Console.ReadLine();
+        }
+        static void triangle(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
